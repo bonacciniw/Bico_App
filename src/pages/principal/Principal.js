c610c2e4fb55c47ef96ad0fe4f61fa73a93f5465
Lucas Souza <49249175+Negrativo@users.noreply.github.com>
2021-09-13 23:51:33 -0300

    Incluso consulta no banco na abertura da tela

--- src/pages/principal/Principal.js
+++ src/pages/principal/Principal.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View, Text, Image, ScrollView, TextInput} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, ScrollView, TextInput, FlatList} from 'react-native';
 
 import EmpregoList from '../../Componentes/EmpregoList';
 import Styles from '../../Styles/StylesAbasPrincipais';
@@ -7,19 +7,28 @@ import iconPesquisa from '../../../assets/pesquisar.png'
 import api from '../../services/api';
 
 export default function({ navigation }) {
+    const[Dados, setDados] = useState('')
 
+    useEffect(() => {
+        api.get('/principal/lista', {})
+            .then(response => {
+                setDados(response.data);
+                //console.log(Dados);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }); 
     
-    /*const response = api.get('/profissional/groupProfissionais', {})
-    .then(response => {
-        return response.json();
-    })
-    .catch(error => {
-        console.log(error);
-    })
-    .then(json => {
-        const Cadastros = json.
-    });*/
+    
 
+    /*
+    <FlatList
+                data={Dados}
+                renderItem={({item}) => <EmpregoList nome={item.nome} local="" emprego="teste"/>}
+                keyExtractor={item._id}
+            /> 
+    */
     return(
         <View style={Styles.container}>
 
@@ -32,13 +41,7 @@ export default function({ navigation }) {
                 </TextInput> 
                 <Image source={iconPesquisa} style={Styles.imagem}/>                 
             </View>
-
-           <ScrollView style={Styles.scrollView}>              
-             <EmpregoList nome="Jose da Silva" local="Ibiporã" emprego="Vendedor(a)"/>
-             <EmpregoList nome="Lucas Souza" local="Londrina" emprego="Programador(a)"/>
-             <EmpregoList nome="Marcos Araujo" local="Londrina" emprego="Garçom"/>
-             <EmpregoList nome="Ana Milena" local="Cambe" emprego="Cuidadora"/>
-           </ScrollView> 
+            
         </View>
     );
 
